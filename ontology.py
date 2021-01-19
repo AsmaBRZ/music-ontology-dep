f66449ae4c52c1bf46f1a6e259e1662a62110be6
Asma <asmabrazi27@gmail.com>
2021-01-19 09:30:09 +0100

    function

--- ontology.py
+++ ontology.py
@@ -47,7 +47,7 @@ def artiste_query(name,birthD,birthP):
 
 def ask_function_musicien(p):
     where = """ASK FROM """
-    where = where+ '<'+str(p) +'>'+ """   { """
+    where = where+ str(p)  + """   { """
 
     where = where  + '{'+str(p)+'  a yago:Musician110339966} UNION {'+str(p)+'  a yago:Musician110340312} UNION {'+str(p)+'  a dbo:Musician} UNION {'+str(p)+' a dbo:Instrumentalist} '  
     where = where + " }"
@@ -56,7 +56,7 @@ def ask_function_musicien(p):
 
 def ask_function_auteur(p):
     where = """ASK FROM """
-    where = where+ '<'+str(p) +'>'+ """   { """
+    where = where+ str(p)  + """   { """
 
     where = where  + '{'+str(p)+'  a dbo:Songwriter} UNION  {'+str(p)+'  a dbo:Writer} UNION {'+str(p)+'  a dbo:Singer-Songwriter} UNION {'+str(p)+' a yago:Songwriter110624540} UNION {'+str(p)+' a yago:Writer110801291} UNION {'+str(p)+'  a yago:Writer110794014} '
     where = where + " }"
@@ -65,7 +65,7 @@ def ask_function_auteur(p):
     
 def ask_function_compositeur(p):
     where = """ASK FROM """
-    where = where+ '<'+str(p) +'>'+ """   { """
+    where = where+ str(p)  + """   { """
 
     where = where  + '{'+str(p)+'  a yago:Composer109947232} UNION {'+str(p)+' a dbo:composer} . '
     where = where + " }"
@@ -73,7 +73,7 @@ def ask_function_compositeur(p):
     
 def ask_function_interprete(p):
     where = """ASK FROM """
-    where = where+ '<'+str(p) +'>'+ """   { """
+    where = where+ str(p)  + """   { """
     
     where = where  + '{'+str(p)+' a dbo:Singer} UNION {'+str(p)+' a dbo:Singer-Songwriter} UNION  {'+str(p)+' a yago:Singer110599806} UNION {'+str(p)+' a yago:Performer110415638} '
     where = where + " }"
@@ -138,7 +138,7 @@ def displayResult(v_nom, v_date, v_place, v_musicien, v_auteur, v_compositeur, v
             d[str(attribut)] = str(valeur)
             
             if key == 'name':
-                url = d['URL']
+                url = '<'+d['URL']+'>'
                 nom_artiste = valeur
                 ask_musicien = ask_function_musicien(url)
                 print("ask_musicien ",ask_musicien)
