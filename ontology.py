edcf9179929803eacd9f26bfdf3605f8955df632
Asma <asmabrazi27@gmail.com>
2021-01-19 06:16:51 +0100

    correct query_artist function

--- ontology.py
+++ ontology.py
@@ -12,7 +12,7 @@ PREFIX dbpedia2: <http://dbpedia.org/property/>
 PREFIX dbpedia: <http://dbpedia.org/>
 PREFIX skos: <http://www.w3.org/2004/02/skos/core#>"""
 
-equivalence =  {"birthP":"Place de naissance","birthD":"Date de naissance", "group":"URL","person":"URL", "name":"Nom", "dateC":"Date de création","origin":"Pays"}
+equivalence =  {"birthP":"Place de naissance","interprete":"Interprète", "compositeur":"Compositeur", "auteur":"Auteur", "musicien":'Musicien', "birthD":"Date de naissance", "group":"URL","person":"URL", "name":"Nom", "dateC":"Date de création","origin":"Pays"}
 def artiste_query(name,birthD,birthP, musicien, auteur, compositeur, interprete):
     select = """SELECT DISTINCT ?person """
 
@@ -33,32 +33,71 @@ def artiste_query(name,birthD,birthP, musicien, auteur, compositeur, interprete)
         where = where  + '?person dbo:birthDate ?birthD . '
 
     if birthP != None :
-        where = where  + '?person dbo:birthPlace "' + birthP + ' " . '
+        where = where  + '?person dbo:birthPlace ?birthP .'
+        where = where  + 'FILTER (?birthPlace = <http://dbpedia.org/resource/'+str(birthP)+'>)'
     else: 
         select = select + " ?birthP "
         where = where  + '?person dbo:birthPlace ?birthP . '
+
+    if musicien:
+        where = where  + '{?person a yago:Musician110339966} UNION {?person a yago:Musician110340312}'
+
+    if auteur:
+        where = where  + '{?person a yago:Songwriter110624540} UNION {?person a yago:Writer110801291} UNION {?person a yago:Writer110794014}'
+
+    if interprete:
+        where = where  + '{?person a yago:Singer110599806} UNION {?person a yago:Performer110415638}'
+
+    if compositeur:
+        where = where  + '{?person a yago:Composer109947232} '
+    
 
     where = where + " } LIMIT 10"
 
     return select + " " + where
     
-def displayResult(v_nom, v_date, v_place, musicien, auteur, compositeur, interprete):
-    global texte1
-    global texte0
-    date_artiste = v_date
-    nom_artiste = v_nom
+def displayResult(v_nom, v_date, v_place, v_musicien, v_auteur, v_compositeur, v_interprete):
+    if v_nom == '':
+        nom_artiste = None
+    else:
+        nom_artiste = v_nom
 
+
+    if v_date == '':
+        date_artiste = None
+    else:
+        date_artiste = v_date
+
+    if v_place == '':
+        lieu_artiste = None
+    else:
+        lieu_artiste = v_place
+
+    if v_musicien == 'musicien':
+        musicien = True
+
+    if v_auteur == 'auteur':
+        auteur = True
+
+    if v_compositeur == 'compositeur':
+        compositeur = True
+
+    if v_interprete == 'interprete':
+        interprete = True
+    
 
     sparql = SPARQLWrapper("http://dbpedia.org/sparql")
     
-
-    query=artiste_query(nom_artiste,date_artiste,  v_place, musicien, auteur, compositeur, interprete)
-
+    query=artiste_query(nom_artiste,date_artiste, lieu_artiste, musicien, auteur, compositeur, interprete)
+    print("\n",query)
     sparql.setQuery(query)
     sparql.setReturnFormat(JSON)
     results = sparql.query().convert()['results']['bindings']
 
     d = {}
+    print("---------------------")
+    print(results)
+    
     for result in results :
         for key,value in result.items():
             attribut = equivalence[key]
